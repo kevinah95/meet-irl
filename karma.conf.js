a77dc22af848be4419b0b5d59c17a6ad79baf32c
Kevin Hernandez Rostran <kevinah95@gmail.com>
2017-03-24 05:20:57 +0100

    Chrome for local, Firefox for Travis

--- karma.conf.js
+++ karma.conf.js
@@ -2,73 +2,76 @@
 // Generated on Wed Mar 22 2017 15:19:17 GMT-0600 (Central America Standard Time)
 
 module.exports = function(config) {
-  config.set({
+    var configuration = {
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-        './node_modules/angular/angular.js',                             // angular
-        './node_modules/angular-ui-router/release/angular-ui-router.js', // ui-router
-        './node_modules/angular-mocks/angular-mocks.js',                 // loads our modules for tests
-        './app/services/users/users.js',                                 // our Users factory
-        './app/app.js',                                                  // our angular app
-        './app/services/users/users.spec.js'                             // our test file for our Users factory
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            './node_modules/angular/angular.js', // angular
+            './node_modules/angular-ui-router/release/angular-ui-router.js', // ui-router
+            './node_modules/angular-mocks/angular-mocks.js', // loads our modules for tests
+            './app/services/users/users.js', // our Users factory
+            './app/app.js', // our angular app
+            './app/services/users/users.spec.js' // our test file for our Users factory
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // list of files to exclude
+        exclude: [],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['spec'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Firefox'];
+    };
+    config.set(configuration);
+};
